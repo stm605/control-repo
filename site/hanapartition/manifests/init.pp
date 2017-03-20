ffc5688f760fb493177d32ac4fd049bac77ab488
Ross Sponholtz <rsponholtz@hotmail.com>
2017-03-20 12:55:27 -0500

    added partitioning code

--- site/hanapartition/manifests/init.pp
+++ site/hanapartition/manifests/init.pp
@@ -44,5 +44,112 @@
 #
 class hanapartition {
 
+include selinux
+include lvm
+#include AlexCline::mounts
+
+
+physical_volume { '/dev/sdc':
+  ensure => present,
+}
+
+physical_volume { '/dev/sdd':
+  ensure => present,
+}
+
+physical_volume { '/dev/sde':
+  ensure => present,
+}
+
+physical_volume { '/dev/sdf':
+  ensure => present,
+}
+
+volume_group { 'hanavg':
+  ensure           => present,
+  physical_volumes => ['/dev/sdc', '/dev/sdd', '/dev/sde', '/dev/sdf'],
+}
+
+logical_volume { 'datalv':
+  ensure       => present,
+  volume_group => 'hanavg',
+  size         => '700G',
+}
+
+logical_volume { 'loglv':
+  ensure       => present,
+  volume_group => 'hanavg',
+  size         => '300G',
+}
+
+logical_volume { 'hanasharedlv':
+  ensure       => present,
+  volume_group => 'hanavg',
+  size         => '300G',
+}
+
+logical_volume { 'usrsaplv':
+  ensure       => present,
+  volume_group => 'hanavg',
+  size         => '200G',
+}
+
+filesystem { '/dev/hanavg/hanasharedlv':
+  ensure  => present,
+  fs_type => 'xfs',
+  options => '-b size=4096 -d su=64k,sw=2',
+}
+
+filesystem { '/dev/hanavg/datalv':
+  ensure  => present,
+  fs_type => 'xfs',
+  options => '-b size=4096 -d su=64k,sw=2',
+}
+
+filesystem { '/dev/hanavg/loglv':
+  ensure  => present,
+  fs_type => 'xfs',
+  options => '-b size=4096 -d su=64k,sw=2',
+}
+
+filesystem { '/dev/hanavg/usrsaplv':
+  ensure  => present,
+  fs_type => 'xfs',
+  options => '-b size=4096 -d su=64k,sw=2',
+}
+
+mounts { 'Mount point for hanashared':
+  ensure => present,
+  source => '/dev/hanavg/hanasharedlv',
+  dest   => '/hana/shared',
+  type   => 'xfs',
+  opts   => 'nofail,defaults,noatime',
+}
+
+mounts { 'Mount point for datalv':
+  ensure => present,
+  source => '/dev/hanavg/datalv',
+  dest   => '/hana/data',
+  type   => 'xfs',
+  opts   => 'nofail,defaults,noatime',
+}
+
+
+mounts { 'Mount point for loglv':
+  ensure => present,
+  source => '/dev/hanavg/loglv',
+  dest   => '/hana/log',
+  type   => 'xfs',
+  opts   => 'nofail,defaults,noatime',
+}
+
+
+mounts { 'Mount point for sap':
+  ensure => present,
+  source => '/dev/hanavg/usrsaplv',
+  dest   => '/usr/sap',
+  type   => 'xfs',
+  opts   => 'nofail,defaults,noatime',
+}
 
 }
