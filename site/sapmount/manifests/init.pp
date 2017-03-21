94ab4023c2054ffc912bc524fddbf94540823973
Ross Sponholtz <rsponholtz@hotmail.com>
2017-03-20 21:15:40 -0500

    modified mount

--- site/sapmount/manifests/init.pp
+++ site/sapmount/manifests/init.pp
@@ -46,12 +46,24 @@ class sapmount {
 
 package { 'cifs-utils': ensure => 'installed' }
 
+#mounts { 'sapmount':
+#  ensure => present,
+#  source => '\\\\sapbitseastus2.file.core.windows.net\\linuxsapbits',
+#  dest   => '/mnt/sapbits',
+#  type   => 'cifs',
+#  opts   => 'vers=3.0,dir_mode=0777,file_mode=0777,username=sapbitsus2,password="xFN8vqjuNmZJylg4y++fEpM4WeJAd3jLMiA1zlJtGik5n1aKgWo/Vk0pXg1h9ke37XI8USw88Eq0gE/2zrtmuQ=="',
+#}
+
 mounts { 'sapmount':
   ensure => present,
-  source => '\\\\sapbitseastus2.file.core.windows.net\\linuxsapbits',
+  source => '//sapbitseastus2.file.core.windows.net/linuxsapbits',
   dest   => '/mnt/sapbits',
   type   => 'cifs',
-  opts   => 'vers=3.0,dir_mode=0777,file_mode=0777,username=sapbitsus2,password="xFN8vqjuNmZJylg4y++fEpM4WeJAd3jLMiA1zlJtGik5n1aKgWo/Vk0pXg1h9ke37XI8USw88Eq0gE/2zrtmuQ=="',
+  opts   => 'vers=3.0,dir_mode=0777,file_mode=0777,username=sapbitseastus2,password=xFN8vqjuNmZJylg4y++fEpM4WeJAd3jLMiA1zlJtGik5n1aKgWo/Vk0pXg1h9ke37XI8USw88Eq0gE/2zrtmuQ==',
 }
+
+#sudo mount -t cifs //sapbitseastus2.file.core.windows.net/linuxsapbits
+#/mnt/sapbits -o vers=3.0,username=sapbitseastus2,password=xFN8vqjuNmZJylg4y++fEpM4WeJAd3jLMiA1zlJtGik5n1aKgWo/Vk0pXg1h9ke37XI8USw88Eq0gE/2zrtmuQ==,dir_mode=0777,file_mode=0777
+#
 
 }
