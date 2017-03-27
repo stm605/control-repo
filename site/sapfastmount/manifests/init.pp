73e241d817455fbc70697e9f9c83cddd6bcc804f
Ross Sponholtz <rsponholtz@hotmail.com>
2017-03-27 14:33:31 -0500

    fixed syntax error

--- site/sapfastmount/manifests/init.pp
+++ site/sapfastmount/manifests/init.pp
@@ -95,5 +95,6 @@ exec { "extract linuxsapbits.tar" :
    creates => "/mnt/sapbits/HANA_51051151",
    cwd => "/mnt/sapbits",
     path    => '/bin:/usr/bin:/usr/sbin',
-     timeout => '0',
+    timeout => '0',
+    }
 }
